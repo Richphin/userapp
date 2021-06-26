1be2a0196636489a0a4427da428bad6d8c07e8aa
Richie <richphinsalah6@gmail.com>
2021-06-26 15:48:50 +0000

    refactor .then .catch to use async await

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -2,62 +2,66 @@ import React from 'react'
 export  function getAllUsers() {
      return(dispatch,state,{getFirestore})=>{
         const db = getFirestore();
-        db.collection('users')
-        .get()
-        .then((results)=>{
-            let users =[];
-            results.forEach((doc)=>{
-                users.push(doc.data());
-            });
-            dispatch({
-                type: 'ADD_ALL_USERS',
-                payload: users,
-              });
-        })
-        .catch((err) => {
-            console.log(err);
-          });
+        db.collection('users').onSnapshot(
+            (results)=>{
+                let users =[];
+                results.forEach((doc)=>{
+                    users.push({...doc.data(), id: doc.id});
+                });
+                dispatch({
+                    type: 'ADD_ALL_USERS',
+                    payload: users,
+                  });
+            },
+            (err) => {
+                console.log(err);
+              }
+        );   
      };
     
 }
 
 
 export function addUser(newUser) {
-    return(dispatch,state,{getFirestore})=>{
+    return async (dispatch,state,{getFirestore})=>{
         const db = getFirestore();
-        db.collection('users')
-        .add(newUser)
-        .then(()=>{
-            dispatch(
-                {
-                    type: "ADD_USER",
-                    payload: newUser,
-                }
-            );
-        })
-        .catch((err) => {
-            console.log(err);
-          });
-
-    }
-
-     
+        try {
+           await db.collection('users').add(newUser)  
+        } catch (error) {
+            console.log(error);
+        }
+    }  
 }
 
 
 export  function deleteUser(id) {
-     
-     return{
-        type:"DELETE_USER",
-        payload: id
-     }
+    return async (dispatch,state,{getFirestore})=>{
+        const db = getFirestore();
+        try {
+           await db.collection('users').doc(id).delete();
+        } catch (error) {
+            console.log(error);
+        }
+    } 
+    //  return{
+    //     type:"DELETE_USER",
+    //     payload: id
+    //  }
  }
  
  export  function updateUser(id,updateduser) {
-     
-    return{
-       type:"UPDATE_USER",
-       payload:{id:id, updatedUserInfo:updateduser}
+    return async (dispatch,state,{getFirestore})=>{
+        const db = getFirestore();
+        try {
+           await db.collection('users').doc(id).update(updateduser);
+        } catch (error) {
+            console.log(error);
+        }
     }
+     
+    // return{
+    //    type:"UPDATE_USER",
+    //    payload:{id:id, updatedUserInfo:updateduser}
+    // }
 }
  
